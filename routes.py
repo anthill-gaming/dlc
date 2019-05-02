326fec25ea29d89dc6c16a552d3095f07a7e7d88
Vladimir <x55aah@gmail.com>
2019-05-02 18:05:31 +0300

    updates

--- routes.py
+++ routes.py
@@ -2,9 +2,3 @@
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
 from dlc import handlers
-from dlc.api.compat.rest import routes as rest_routes
-from anthill.framework.utils.urls import include
-
-route_patterns = [
-    url(r'^/', include(rest_routes.route_patterns, namespace='api')),  # for compatibility only
-]
