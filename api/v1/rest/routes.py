0a487d67202b1897936d09951ad33ee1c85d475b
Vladimir <x55aah@gmail.com>
2019-07-19 17:30:32 +0300

    updates

--- api/v1/rest/routes.py
+++ api/v1/rest/routes.py
@@ -3,9 +3,10 @@
 from tornado.web import url
 from . import handlers
 
+
 route_patterns = [
     url(r'^/bundle/(?P<id>[^/]+)/?$', handlers.BundleHandler, name='bundle'),
     url(r'^/bundle/?$', handlers.BundleHandler, name='bundle_create'),
-    url(r'^/data/(?P<app_name>[^/]+)/(?P<app_version>[^/]+)/?$',
+    url(r'^/bundles/(?P<app_name>[^/]+)/(?P<app_version>[^/]+)/?$',
         handlers.BundlesHandler, name='bundles'),
 ]
